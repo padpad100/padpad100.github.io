ead6b1e8a2d4ea8c98ef668aa0f3b1b4d8fb81a8
padpad100 <160401041+padpad100@users.noreply.github.com>
2024-02-18 17:16:36 +0300

    Add files via upload

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,56 @@
+export const metadata = {
+  title: 'Sign Up - Simple',
+  description: 'Page description',
+}
+
+import Link from 'next/link'
+
+export default function SignUp() {
+  return (
+    <section className="bg-gradient-to-b from-gray-100 to-white">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h1 className="h1">Добро пожаловать!</h1>
+          </div>
+
+          {/* Form */}
+          <div className="max-w-sm mx-auto">
+            <form>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Имя <span className="text-red-600">*</span></label>
+                  <input id="name" type="text" className="form-input w-full text-gray-800" placeholder="Введите ваше имя" required />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
+                  <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Введите вашу почту" required />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Пароль <span className="text-red-600">*</span></label>
+                  <input id="password" type="password" className="form-input w-full text-gray-800" placeholder="Введите пароль" required />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mt-6">
+                <div className="w-full px-3">
+                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Зарегстрироваться</button>
+                </div>
+              </div>
+              <div className="text-sm text-gray-500 text-center mt-3">
+                By creating an account, you agree to the <a className="underline" href="#0">terms & conditions</a>, and our <a className="underline" href="#0">privacy policy</a>.
+              </div>
+            </form>
+          
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/landing-page-example',
+  output: "export",
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
